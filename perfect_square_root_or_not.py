450d206ca47788580fd01db8642fbd14b8b49571
Murthy92 <97426500+Murthy92@users.noreply.github.com>
2022-05-22 20:28:45 +0530

    num=int(input()) c=0 i=0 while(True):     if(i*i>num):         break     elif(i*i==num):         c+=1         break     i+=1 if(c==1):     print("True") else:     print("False")

---
+++ perfect_square_root_or_not.py
@@ -0,0 +1,14 @@
+num=int(input())
+c=0
+i=0
+while(True):
+    if(i*i>num):
+        break
+    elif(i*i==num):
+        c+=1
+        break
+    i+=1
+if(c==1):
+    print("True")
+else:
+    print("False")
